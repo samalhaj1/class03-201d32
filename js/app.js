8ba7164ef7deff6f3951928e9c806c5284ae6347
samalhaj1 <samalhaj627@gmail.com>
2021-06-23 01:50:56 +0300

    add new comment

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-'use strict'
+'use strict';
 alert('Welcome to my page')
 
 let username = prompt('What is your name?');
@@ -49,11 +49,11 @@ username3 = prompt('your answer is wrong');
 }
 
     
-let userchamp = prompt(' how many champions league cup does real madrid won?');
 
-let guess = 1;
+
+let guess=0;
 while ( guess < 4) {
-
+let userchamp = prompt(' how many champions league cup does real madrid won?');
 if ( userchamp == 13)
 {
     alert ( 'correct');
@@ -61,12 +61,14 @@ if ( userchamp == 13)
 }
 else if ( userchamp >= 17 ){
 alert("too high");
+guess++;
 }
 else if ( userchamp <=10 ){
     alert("too low");
+    guess++;
     }
 
-    else if ( guess == 4) {
+if ( guess == 4) {
 
     alert ( ' you have finishied all of your tries , the correct answer is 13');
         break;
@@ -75,13 +77,12 @@ else if ( userchamp <=10 ){
 
         alert ( 'try again ');
     }
-    guess++;
-userchamp = prompt(' how many champions league cup does real madrid won?')
+
 
 }
- let maxtries = 6;
+ let maxtries = 5;
 let rewards = ['2017', '2018','2016'];
-for (var i=0; i<maxtries;i++){
+for (var i=0; i< 5 ;i++){
 
     let username6 = prompt(' name one of the years that Real Madrid won a champions leage cup?');
     if (username6 == rewards [0]){alert ('correct');
@@ -94,14 +95,15 @@ for (var i=0; i<maxtries;i++){
 else if (username6 == rewards [2]){alert ('correct');
 break;
 }
-else if (i == maxtries)
+ if (maxtries == 1)
 {
     alert ('you have finished your tries the correwct answer is '+rewards);
-    
+    break;
 
 }
 else {
     alert (' try again ');
+    maxtries--;
 }
 
 
